0d8bf103d63f62f35f9ea2c4265f16f1b493c6a5
Adarsha Poudel <adarsha@ucsb.edu>
2020-05-11 01:56:34 -0700

    hey

--- NaiveBayesClassifier.cpp
+++ NaiveBayesClassifier.cpp
@@ -2,33 +2,150 @@
 #include <sstream>
 #include <fstream>
 #include "NaiveBayesClassifier.h"
+#include <math.h>
+#include <chrono>
 
 using namespace std;
 
-
-NaiveBayesClassifier::NaiveBayesClassifier()
-{
-    size = 0;
-    positiveReview = 0;
-    negativeReveiw = 0;
-    trainingTime = 0;
-    entries = 0;
-
-}
-
 NaiveBayesClassifier::NaiveBayesClassifier(string fileName){
+    
     ifstream in(fileName);
     string line;
+    negativeReview = 0;
+    positiveReview = 0;
+    kNegative = 0;
+    kPositve = 0;
+    p = 0;
+    n = 0;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
     if(in.is_open()){
+        
+        entries = new list<Label>[size];
         while (getline(in, line)){
-            cout<< line<<endl;
+        
+            int label= line[line.size()-1] - '0';
+            
+
+            istringstream ss(line.substr(0, line.size()-3));
+            string token;
+            if(label == 0){
+                n++;
+            }else{
+                p++;
+            }
+
+            while(getline(ss, token, ' '))
+                    this->insert(token, label);
+           
         }
         in.close();
+        
+    } else{
+        cerr << "Can't open the file"<< endl;
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    cout<< "Trianing time: "<< duration<<endl; 
+}
+
+
+void NaiveBayesClassifier::insert(string word, int label){
+    int index = this->hash(word);
+    for (list<Label>::iterator it = entries[index].begin(); it != entries[index].end(); ++it){
+        if((it)-> word == word){
+            if(label == 1){
+                if(it->positiveCount == 0) kPositve++;
+                (it->positiveCount)++;
+                positiveReview++;
+            } else{
+                if(it->negativeCount == 0) kNegative++;
+                (it->negativeCount)++;
+                negativeReview++;
+            }
+            return;
+        }
+    }
+    if(label == 0){
+        Label l(word, 0, 1);
+        entries[index].push_front(l);
+        kNegative ++;
+        negativeReview++;
+      
+    }else{
+        Label l(word, 1, 0);
+        entries[index].push_front(l);
+        kPositve++;
+        positiveReview++;
     }
 }
 
+int NaiveBayesClassifier::hash(string word){
+    unsigned int hashValue = 1;
+    for (int i = 0; i < word.size(); i++){
+        hashValue = hashValue* word[i];
+    }
+    return hashValue % this->size; 
+}
+
+double NaiveBayesClassifier::returnProbability(string word, int label){
+    int index = this->hash(word);
+
+    for (list<Label>::iterator it = entries[index].begin(); it != entries[index].end(); ++it){
+        if((it)-> word == word){
+            if(label == 0){
+                return (it->negativeCount + 1.0)/ (negativeReview + kPositve);
+            }else{
+                return (it->positiveCount + 1.0)/ (positiveReview + kNegative);
+            }
+        }
+    }
+    return 0;
+}
+
 void NaiveBayesClassifier::test(string fileName){
-    cout<< "Testing:"<< fileName<< endl;
+    double accuracy = 0;
+    double totalReviews = 0.0;
+    ifstream in(fileName);
+    string line;
+  
+    double pPositiveReview = p * 1.0 / (p + n); 
+    double pNegativeReview = n * 1.0 / (p + n); 
+
+    if(in.is_open()){
+        while (getline(in, line)){
+            totalReviews++;
+            double probPos = log(pPositiveReview); 
+            double probNeg = log(pNegativeReview);
+
+            int label = line[line.size()-1] - '0';
+            
+            istringstream ss(line.substr(0, line.size()-3));
+            string token;
+
+
+           while(getline(ss, token, ' ')){
+                    probPos += log(returnProbability(token, 1));
+                    probNeg += log(returnProbability(token, 0));
+            
+            }
+           
+            if(probNeg > probPos){
+                if(label == 0) accuracy ++;
+                //cout << 0;
+            }else{
+                if(label ==1) accuracy ++;
+               // cout<< 1;
+            }
+        }
+        in.close();
+    } else{
+        cerr << "Can't open the file"<< endl;
+    }
+    cout<< "accuracy " <<accuracy<<endl;
+    cout<< "totalReviews " << totalReviews <<endl;
+    accuracy = accuracy/ totalReviews;
+    cout<< "Accuary: "<< accuracy<<endl;
 }
 
 int main(int argc, char** argv){
@@ -37,4 +154,5 @@ int main(int argc, char** argv){
     }
     NaiveBayesClassifier n(argv[1]);
     n.test(argv[2]);
+   // n.print();
 }
--- NaiveBayesClassifier.h
+++ NaiveBayesClassifier.h
@@ -2,27 +2,40 @@
 #define NaiveBayesClassifier_H
 #include <string>
 #include <list>
+using namespace std;
 
-struct label{
+struct Label{
     std::string word; 
     int positiveCount;
     int negativeCount;
-    label(std::string w, int p = 0, int n = 0): word(w), positiveCount(p), negativeCount(n){}
+    Label(std::string w, int p = 0, int n = 0): word(w), positiveCount(p), negativeCount(n){}
 };
 class NaiveBayesClassifier{
     private:
-        int size;
-        std::list<label>* entries;
+        const int size = 6577;
+        std::list<Label>* entries;
         int positiveReview;
-        int negativeReveiw;
-        int trainingTime; 
+        int negativeReview;
+        int p;
+        int n;
+        int kPositve;
+        int kNegative;
+        double returnProbability(std::string, int label);
 
     public:
-        NaiveBayesClassifier(std::string); //default constructor is used for training
-        NaiveBayesClassifier();
-        void insert(label);
+        NaiveBayesClassifier(std::string); //constructor is used for training
+        void insert(std::string, int);
         int hash(std::string);
         void test(std::string);
+
+        void print(){
+            for(int i =0; i < size; i++){
+                for (std::list<Label>::iterator it = entries[i].begin(); it != entries[i].end(); ++it){
+                   std::cout<< (it)->word << " " << (it)->positiveCount <<  " " << (it)->negativeCount << "\n";
+                }
+               // std::cout << "__________________________________________________________\n";
+            }
+        }
 
 };
 #endif
